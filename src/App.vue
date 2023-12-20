ac05e817a64d3cf7efb6a9112ea9b7388589ce95
Delanno Kotcho <delanktc24@gmail.com>
2023-12-20 16:51:33 +0100

    mise en place du header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1 class="text-red-500 text-2xl font-bold">Hello World</h1>
+
+  <HeaderComp />
+
+  <main>
+
+  </main>
 </template>
+
+<script setup lang="ts">
+  import HeaderComp  from "@/components/HeaderComp.vue";
+</script>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+       
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Trending</a>
+                </li>
+                <li>
+                    <a href="">Verified</a>
+                </li>
+                <li>
+                    <a href="">Collections</a>
+                </li>
+                <li>
+                    <a href="">Search</a>
+                </li>
+                <li>
+                    <a href="">Account</a>
+                </li>
+            </ul>
+            <p>HULU</p>
+       
+    </header>
+</template>
